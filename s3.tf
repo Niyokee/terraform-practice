03a6073316f37e84ac32bbf8742dbd5e135c3ac6
noaki-k <knk43nok@gmail.com>
2022-04-26 08:34:41 +0900

    add: s3

---
+++ s3.tf
@@ -0,0 +1,74 @@
+resource "aws_s3_bucket" "private" {
+  bucket = "private-pragmatic-terraform-2022-04-26"
+}
+
+resource "aws_s3_bucket_versioning" "private" {
+  bucket = aws_s3_bucket.private.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "private" {
+  bucket = aws_s3_bucket.private.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform-2022-04-26"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "public" {
+  bucket = aws_s3_bucket.public.bucket
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://example.com"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform-2022-04-26"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+principals {
+      identifiers = ["893095117949"]
+      type        = "AWS"
+    }
+  }
+}
