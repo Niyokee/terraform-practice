7e1ae3d5cec0863db8a0abefee7a6b862afa8864
noaki-k <knk43nok@gmail.com>
2022-05-03 15:07:49 +0900

    add: ecs-task-scheduler

--- ecs.tf
+++ ecs.tf
@@ -12,6 +12,16 @@ resource "aws_ecs_task_definition" "example" {
   execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
 }
 
+resource "aws_ecs_task_definition" "example_batch" {
+  family                   = "example-batch"
+  cpu                      = "256"
+  memory                   = "512"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./batch_container_definition.json")
+  execution_role_arn       = module.ecs_task_execution_role.iam_role_arn
+}
+
 resource "aws_ecs_service" "example" {
   name                              = "example"
   cluster                           = aws_ecs_cluster.example.arn
@@ -53,6 +63,10 @@ data "aws_iam_policy" "ecs_task_execution_role_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+data "aws_iam_policy" "ecs_events_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
+}
+
 data "aws_iam_policy_document" "ecs_task_execution" {
   source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
 
@@ -68,4 +82,11 @@ module "ecs_task_execution_role" {
   name       = "ecs-task-execution"
   identifier = "ecs-tasks.amazonaws.com"
   policy     = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
+module "ecs_events_role" {
+  source     = "./iam_role"
+  name       = "ecs-events"
+  identifier = "events.amazonaws.com"
+  policy     = data.aws_iam_policy.ecs_events_role_policy.policy
 }
